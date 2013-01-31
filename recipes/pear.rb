02c03a9a989a4907b18b7821d0c25ca0324864f8
David Joos <david.joos@gmail.com>
2013-01-31 17:49:39 +0000

    Fixes

--- recipes/pear.rb
+++ recipes/pear.rb
@@ -8,13 +8,29 @@
 include_recipe "php"
 
 #PHP Extension and Application Repository PEAR channel
-php_pear_channel "pear.php.net" do
+pearhub_chan = php_pear_channel "pear.php.net" do
   action :update
 end
 
 #upgrade PEAR
 php_pear "PEAR" do
+	channel pearhub_chan.channel_name
 	action :upgrade
+end
+
+#eZ Enterprise components PEAR channel
+php_pear_channel "components.ez.no" do
+	action :discover
+end
+
+#Netpirates PEAR channel
+php_pear_channel "pear.netpirates.net" do
+	action :discover
+end
+
+#Symfony2 PEAR channel
+php_pear_channel "pear.symfony.com" do
+	action :discover
 end
 
 #PHPUnit PEAR channel
