36751106f6cf341d2526fb0852ca17d581eface5
David Joos <david.joos@gmail.com>
2013-04-25 09:30:39 +0100

    add installation method for phar archive fixes #2

--- attributes/default.rb
+++ attributes/default.rb
@@ -7,10 +7,3 @@
 
 default[:phploc][:install_method] = "pear"
 default[:phploc][:version] = "latest"
-
-#composer install only
-default[:phploc][:prefix] = "/usr/bin"
-
-#phar install only
-default[:phploc][:phar_url] = "http://pear.phpunit.de/get/phploc.phar"
-default[:phploc][:install_dir] = ""
--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "dev@escapestudios.com"
 license          "MIT"
 description      "Installs/Configures phploc"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.0.3"
+version          "0.0.4"
 
 %w{ debian ubuntu redhat centos fedora scientific amazon }.each do |os|
 supports os
@@ -16,3 +16,4 @@ depends "composer"
 recipe "phploc", "Installs phploc."
 recipe "phploc::composer", "Installs phploc using composer."
 recipe "phploc::pear", "Installs phploc using pear."
+recipe "phploc::phar", "Installs phploc using phar."
