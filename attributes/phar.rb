46e5fe88709ed1e4ebb2b92e698cadd5777f3276
Dieter Blomme <dieterblomme@gmail.com>
2015-02-04 14:24:46 +0100

    Refactor for configurable install location

--- attributes/phar.rb
+++ attributes/phar.rb
@@ -5,5 +5,4 @@
 # Copyright (c) 2016, David Joos
 #
 
-default['phploc']['phar_url'] = 'https://phar.phpunit.de/phploc.phar'
-default['phploc']['install_dir'] = '/usr/bin'
+default['phploc']['phar_url'] = 'https://phar.phpunit.de/phpunit.phar'
--- recipes/phar.rb
+++ recipes/phar.rb
@@ -5,7 +5,7 @@
 # Copyright (c) 2016, David Joos
 #
 
-remote_file "#{node['phploc']['install_dir']}/phploc" do
+remote_file "#{node['phploc']['bin_dir']}/phploc" do
   source node['phploc']['phar_url']
   mode 0755
 end
